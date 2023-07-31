fa52ee95cb624652a07bd60477aa8059c6e94801
isuri <isuriwgunaratne@gmail.com>
2023-08-01 01:25:44 +0530

    Complete Task-a

---
+++ Task_a.java
@@ -0,0 +1,8 @@
+public class Task_a{
+    public static void main(String[] args) {
+        
+        //print the given figure.
+        System.out.printf("\u001B[31m%10s%d\u001B[32m%d%%10s \n","",5,0,"");
+    }
+    
+}
